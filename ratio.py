b2f941010c017557d584608b967d853e7b4a4bdf
EugeneL-9929 <yliujz@connect.ust.hk>
2026-02-11 16:52:22 +0800

    change

---
+++ ratio.py
@@ -0,0 +1,8 @@
+import numpy as np
+import requests as r
+
+class Ratio():
+    def __init__(self):
+        self.base_url = "hk.investing.com/equities/apple-computer-inc-ratios"
+
+print("hello, ratio!")
