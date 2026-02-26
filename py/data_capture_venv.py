42b57f9a4102a4fce019c7d58d33a21da279106d
EugeneL-9929 <yliujz@connect.ust.hk>
2026-02-26 15:59:29 +0800

    change

--- py/data_capture_venv.py
+++ py/data_capture_venv.py
@@ -3,7 +3,16 @@ import json
 import os
 from pathlib import Path
 
-# with open(os.path.join(Path(__file__).parent, 'data', 'I_url.json')) as jsonFile:
-#     data = json.load(jsonFile)
+with open(os.path.join(Path(__file__).parent.parent, 'data', 'I_url.json')) as jsonFile:
+    data = json.load(jsonFile)
+    print(data)
+
+class Stock():
+    def __init__(self, *, stock_code, **kwargs):
+        self.url = data['stock_url']
+    
+    def initiate_data(self):
+        session = requests.Session(impersonate='chrome120')
+        
 
 print('data_capture.py')
